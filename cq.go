d26470e8805cce3e1881d5fb178e607356470b32
Florian Thienel <ftl@thecodingflow.com>
2022-08-28 15:02:32 +0200

    add support for CQ-WPX contests

--- cq.go
+++ cq.go
@@ -3,6 +3,13 @@ This file contains the implementation of the specific things for contests announ
 */
 package conval
 
+import (
+	"regexp"
+	"unicode"
+
+	"github.com/ftl/hamradio/callsign"
+)
+
 func init() {
 	PropertyGetters[WPXPrefixProperty] = PropertyGetterFunc(getWPXPrefix)
 }
@@ -12,5 +19,28 @@ const (
 )
 
 func getWPXPrefix(qso QSO) string {
-	return "" // TODO implement
+	return WPXPrefix(qso.TheirCall)
+}
+
+var parseWPXPrefixExpression = regexp.MustCompile(`^[A-Z0-9]?[A-Z][0-9]*`)
+
+func WPXPrefix(call callsign.Callsign) string {
+	var p string
+	if p == "" && call.Prefix != "" {
+		p = parseWPXPrefixExpression.FindString(call.Prefix)
+	}
+	if p == "" && call.Suffix != "" {
+		p = parseWPXPrefixExpression.FindString(call.Suffix)
+	}
+	if p == "" {
+		p = parseWPXPrefixExpression.FindString(call.BaseCall)
+	}
+	if p == "" {
+		return ""
+	}
+	runes := []rune(p)
+	if !unicode.IsDigit(runes[len(runes)-1]) {
+		p += "0"
+	}
+	return p
 }
