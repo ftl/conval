2dd62622b8def561371db3e0e49077cb5c850493
Florian Thienel <ftl@thecodingflow.com>
2026-09-13 12:35:34 +0200

    add rules for SARL-HF-CW

---
+++ custom.go
@@ -0,0 +1,166 @@
+/*
+This file contains the implementation of the contest specific properties:
+- EUDXCC (https://eudxcc.altervista.org/)
+- VERON, the Dutch amateur radio association (https://www.veron.nl)
+- SAC, the Scandinavian Activity Contest (https://www.sactest.net)
+- SARL, the South African Radio League (https://mysarl.org.za)
+*/
+package conval
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/ftl/hamradio/callsign"
+)
+
+func init() {
+	commonPropertyValidators[EURegionProperty] = RegexpValidator(validEURegion, "EU region")
+	commonPropertyValidators[PAProvinceProperty] = RegexpValidator(validPAProvince, "PA province")
+
+	commonPropertyGetters[EURegionProperty] = getTheirExchangeProperty(EURegionProperty)
+	commonPropertyGetters[PAProvinceProperty] = getTheirExchangeProperty(PAProvinceProperty)
+	commonPropertyGetters[VeronEntityProperty] = PropertyGetterFunc(getVeronEntity)
+	commonPropertyGetters[SACAreaProperty] = PropertyGetterFunc(getSACArea)
+	commonPropertyGetters[SARLAreaProperty] = PropertyGetterFunc(getSARLArea)
+
+	myPropertyGetters[PAProvinceProperty] = getMyExchangeProperty(PAProvinceProperty)
+	myPropertyGetters[VeronEntityProperty] = PropertyGetterFunc(getMyVeronEntity)
+}
+
+const (
+	EURegionProperty    Property = "eu_region"
+	PAProvinceProperty  Property = "pa_province"
+	VeronEntityProperty Property = "veron_entity"
+	SACAreaProperty     Property = "sac_area"
+	SARLAreaProperty    Property = "sarl_area"
+)
+
+var (
+	validEURegion   = regexp.MustCompile(`AT0[1-9]|BE[01][0-9]|BG0[1-6]|CZ[01][0-9]|CY0[1-5]|DK0[1-6]|EE0[1-5]|FI[01][0-9]|FR[0-2][0-9]|DE[01][0-9]|GR[01][0-9]|HU0[1-7]|IE0[1-4]|IT[0-2][0-9]|LV0[1-6]|LT0[1-5]|LX01|MT0[1-5]|NL[01][0-9]|PL[01][0-9]|RO0[1-8]|SK0[1-8]|SI0[1-6]|ES[01][1-9]|SE[0-2][1-9]`)
+	validPAProvince = regexp.MustCompile(`DR|FL|FR|GD|GR|LB|NB|NH|OV|UT|ZH|ZL`)
+)
+
+var callAreaDigitExpression = regexp.MustCompile("[0-9]")
+
+func callAreaDigit(call callsign.Callsign) string {
+	prefix := call.Prefix
+	if prefix == "" {
+		prefix = call.BaseCall
+	}
+	if len(prefix) > 1 {
+		// the first character may be a digit that belongs to the prefix, not to the call area
+		prefix = prefix[1:]
+	}
+
+	return callAreaDigitExpression.FindString(prefix)
+}
+
+func getMyVeronEntity(_ QSO, setup Setup, _ PrefixDatabase) string {
+	return VeronEntity(setup.MyCall, setup.MyCountry)
+}
+
+func getVeronEntity(qso QSO, _ Setup, _ PrefixDatabase) string {
+	return VeronEntity(qso.TheirCall, qso.TheirCountry)
+}
+
+func VeronEntity(call callsign.Callsign, dxccEntity DXCCEntity) string {
+	dxccEntity = DXCCEntity(strings.ToUpper(string(dxccEntity)))
+	switch dxccEntity {
+	case "CE", "JA", "LU", "PY", "K", "VK", "ZS", "ZL":
+		return string(dxccEntity) + veronCallAreaNumber(call)
+	case "VE":
+		return CanadianTerritory(call)
+	case "UA9":
+		return "UA" + veronCallAreaNumber(call)
+	default:
+		return string(dxccEntity)
+	}
+}
+
+var veronNumberCallAreaExpression = regexp.MustCompile("[0-9]+")
+
+func veronCallAreaNumber(call callsign.Callsign) string {
+	var number string
+	switch {
+	case call.Prefix != "":
+		number = veronNumberCallAreaExpression.FindString(call.Prefix)
+	case call.Suffix != "":
+		number = veronNumberCallAreaExpression.FindString(call.Suffix)
+	default:
+		number = veronNumberCallAreaExpression.FindString(call.BaseCall[1:])
+	}
+	if number == "" {
+		number = "0"
+	}
+	if len(number) > 1 {
+		number = number[1:]
+	}
+	return number
+}
+
+var canadianPrefixExpression = regexp.MustCompile("[CVX][A-Z][0-9]")
+
+func CanadianTerritory(call callsign.Callsign) string {
+	// according to https://hamwaves.com/map.ca/en/index.html
+	callPrefix := ""
+	if callPrefix == "" && call.Prefix != "" {
+		callPrefix = canadianPrefixExpression.FindString(call.Prefix)
+	}
+	if callPrefix == "" {
+		callPrefix = canadianPrefixExpression.FindString(call.BaseCall)
+	}
+
+	switch callPrefix {
+	case "VO1", "VO2", "VY1", "VY2", "VY9", "VY0", "CY0", "CY9":
+		return callPrefix
+	default:
+		return "VE" + veronCallAreaNumber(call)
+	}
+}
+
+func getSACArea(qso QSO, setup Setup, prefixes PrefixDatabase) string {
+	return sacArea(qso.TheirCall, DXCCEntity(getDXCCEntity(qso, setup, prefixes)))
+}
+
+func sacArea(call callsign.Callsign, dxccEntity DXCCEntity) string {
+	// according to https://www.sactest.net/blog/scandinavian-activity-contest-2025-rules/ §8.2
+	entity := strings.ToLower(string(dxccEntity))
+	if entity == "" {
+		return ""
+	}
+
+	number := callAreaDigit(call)
+	if number == "" {
+		number = "0"
+	}
+	return entity + number
+}
+
+func getSARLArea(qso QSO, setup Setup, prefixes PrefixDatabase) string {
+	return sarlArea(qso.TheirCall, DXCCEntity(getDXCCEntity(qso, setup, prefixes)))
+}
+
+func sarlArea(call callsign.Callsign, dxccEntity DXCCEntity) string {
+	// according to the SARL HF Phone, Digital and CW Contests §6.4, see https://mysarl.org.za/contest-resources/
+
+	// the prefix database resolves ZS7 to Antarctica, not to South Africa
+	if strings.HasPrefix(strings.ToUpper(call.BaseCall), "ZS7") {
+		return "8"
+	}
+
+	switch strings.ToLower(string(dxccEntity)) {
+	case "zs":
+		area := callAreaDigit(call)
+		if area >= "1" && area <= "6" {
+			return area
+		}
+		return "9"
+	case "v5":
+		return "7"
+	case "3da", "7p", "7q", "9j", "c9", "a2", "d2", "z2", "zd7", "zd9", "zs8", "fr", "3b8", "5r", "fh", "d6":
+		return "8"
+	default:
+		return "9"
+	}
+}
---
+++ custom_test.go
@@ -0,0 +1,106 @@
+package conval
+
+import (
+	"testing"
+
+	"github.com/ftl/hamradio/callsign"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVeronEntity(t *testing.T) {
+	tt := []struct {
+		call       string
+		dxccEntity DXCCEntity
+		expected   string
+	}{
+		{"JA1ABC", "JA", "JA1"},
+		{"LU/G3XYZ", "LU", "LU0"},
+		{"W8/G3XYZ", "K", "K8"},
+		{"K5ZD", "K", "K5"},
+		{"K5ZD/1", "K", "K1"},
+		{"XK2ABC", "VE", "VE2"},
+	}
+	for _, tc := range tt {
+		t.Run(tc.call, func(t *testing.T) {
+			actual := VeronEntity(callsign.MustParse(tc.call), tc.dxccEntity)
+
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestCanadianTerritory(t *testing.T) {
+	tt := []struct {
+		call     string
+		expected string
+	}{
+		{"VE2ABC", "VE2"},
+		{"VA2ABC", "VE2"},
+		{"CG2ABC", "VE2"},
+		{"XK2ABC", "VE2"},
+		{"VO1ABC", "VO1"},
+		{"VY1ABC", "VY1"},
+	}
+	for _, tc := range tt {
+		t.Run(tc.call, func(t *testing.T) {
+			actual := CanadianTerritory(callsign.MustParse(tc.call))
+
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestSACArea(t *testing.T) {
+	tt := []struct {
+		call     string
+		entity   DXCCEntity
+		expected string
+	}{
+		{"SM3ABC", "sm", "sm3"},
+		{"SI3ABC", "sm", "sm3"},
+		{"SK3AB", "sm", "sm3"},
+		{"7S3ABC", "sm", "sm3"},
+		{"8S3ABC", "sm", "sm3"},
+		{"LA/G3XYZ", "la", "la0"},
+		{"OZ150A", "oz", "oz1"},
+		{"OZ1ABC", "oz", "oz1"},
+		{"SM5/SM3ABC", "sm", "sm5"},
+		{"OH0XX", "oh0", "oh00"},
+		{"OJ0AM", "oj0", "oj00"},
+		{"JW5E", "jw", "jw5"},
+		{"DL1ABC", "", ""},
+	}
+	for _, tc := range tt {
+		t.Run(tc.call, func(t *testing.T) {
+			assert.Equal(t, tc.expected, sacArea(callsign.MustParse(tc.call), tc.entity))
+		})
+	}
+}
+
+func TestSARLArea(t *testing.T) {
+	tt := []struct {
+		call     string
+		entity   DXCCEntity
+		expected string
+	}{
+		{"ZS1ABC", "zs", "1"},
+		{"ZR1ABC", "zs", "1"},
+		{"ZU1ABC", "zs", "1"},
+		{"ZS2ABC", "zs", "2"},
+		{"ZS6ABC", "zs", "6"},
+		{"ZS0ABC", "zs", "9"},
+		{"ZS7ABC", "ce9", "8"},
+		{"ZS8XX", "zs8", "8"},
+		{"V51ABC", "v5", "7"},
+		{"3DA0AB", "3da", "8"},
+		{"FR5ABC", "fr", "8"},
+		{"D68ABC", "d6", "8"},
+		{"DL1ABC", "dl", "9"},
+		{"K1ABC", "k", "9"},
+	}
+	for _, tc := range tt {
+		t.Run(tc.call, func(t *testing.T) {
+			assert.Equal(t, tc.expected, sarlArea(callsign.MustParse(tc.call), tc.entity))
+		})
+	}
+}
